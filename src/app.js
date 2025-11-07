566c207c0cd50b499228e56f61c61bec65f27d7a
vdzhuteva <v.k.dzhuteva@gmail.com>
2025-11-07 15:36:42 +0200

    Add initial project files including HTML, JavaScript, and API integration

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import * as api from './data/api.js';
+
+window.api = api;
+
---
+++ src/data/api.js
@@ -0,0 +1,34 @@
+const hostname = 'https://parseapi.back4app.com/';
+const appId = 'jWTjxWRGxwEAr7lsago7sEhEapaLBZLpROvDkB3c';
+const apiKey = 'dxSZ2CrHxtt68nc9zhlqphvZTPRUf2PvvuKPnXj5';
+
+async function request(method, url, data) {
+    const options = {
+        method,
+        headers: {
+            'X-Parse-Application-Id': appId,
+            'X-Parse-JavaScript-Key': apiKey
+        }
+    };
+
+    if (data !== undefined) {
+        options.headers['Content-Type'] = 'application/json',
+        options.body = JSON.stringify(data);
+    }
+
+    // Todo add authentication
+
+    const res = await fetch(hostname + url, options);
+
+    // todo response validation
+    
+    if (res.status == 204) {
+        return res;
+    }
+
+}
+
+export const get = (url) => request('get', url);
+export const post = (url, data) => request('post', url, data);
+export const put = (url, data) => request('put', url, data);
+export const del = (url) => request('delete', url);
